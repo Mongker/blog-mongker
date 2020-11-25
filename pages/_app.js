b6e93c1742c3eb5b37978fc8898494242a863094
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-11-26 03:22:24 +0700

    MongLV: Cắt css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { wrapper } from '../redux/store'
 import 'antd/dist/antd.css';
-
+import '../styles/index.css'
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,43 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import HeadView from  '../components/HeadView'
-import { Layout, Menu } from 'antd';
+import HeadView from  '../components/HeadView';
+import { Badge } from 'antd';
+import {MenuOutlined, ShoppingCartOutlined} from '@ant-design/icons';
 
-// styles
-// import styles from '../styles/index.css'
-const { Header, Content, Footer } = Layout;
 function Index() {
-    return(
-        <div className={'root-container'}>
+  const [windowSize, setWindowSize] = React.useState({
+    heightApp: null,
+    widthApp: null,
+  });
+  React.useEffect(() => {
+    setWindowSize({
+      heightApp: window.innerHeight,
+      widthApp: window.innerWidth
+    })
+  }, [])
+  console.log(windowSize.widthApp);
+  console.log(windowSize.heightApp);
+  return(
+        <div className={'controller_page'}>
           <HeadView />
-          <Layout>
-            <Header>
-              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                <Menu.Item key="1">nav 1</Menu.Item>
-                <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>
-              </Menu>
-            </Header>
-            <Content>Content</Content>
-            <Footer>Footer</Footer>
-          </Layout>
-          <style jsx>{`
-            .root-container { font-family: "Times New Roman";}
-            .root-container header .ant-layout-header { padding-left: 50%;}
-          `}</style>
+          <div className={'mobile_header'}>
+            <div className={'mobile_controller_header'}>
+              <div className={'mobile_item_header'}><MenuOutlined /></div>
+              <div className={'mobile_title_header'}>Shop Vân Kelly</div>
+              <Badge count={20} size="small" >
+                <div className={'mobile_item_header'}>
+                  <img width={24} height={24} src="https://salt.tikicdn.com/ts/upload/70/44/6c/a5ac520d156fde81c08dda9c89afaf37.png" alt="icon" />
+                </div>
+              </Badge>
+            </div>
+            <div className={'mobile_search_input'}>
+              <img src="https://salt.tikicdn.com/ts/upload/34/62/0c/6ae13efaff83c66f810c4c63942cf6c0.png" alt="icon-search" />
+              <input aria-label={'search'} className={'search'} type={'search'} placeholder={'Bạn cần tìm gì hôm nay ?'}/>
+            </div>
+            <div className={'mobile_slider_header'} style={{paddingLeft: `${windowSize.widthApp*0.05}px`, paddingRight: `${windowSize.widthApp*0.05}px`}}>
+              <img width={windowSize.widthApp*0.9} heigth={(windowSize.widthApp*0.9)/2}  src="https://salt.tikicdn.com/cache/w828/ts/banner/c3/dc/8f/5459f71a05c87b15d7d98c9a87197d32.jpg" alt="https://tiki.vn/chuong-trinh/wipro-unza" />
+            </div>
+          </div>
         </div>
     );
 }
