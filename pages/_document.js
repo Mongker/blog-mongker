3e2615e9dc02af4b210a3aea1f6884a7565ba959
Mongker <levanmong.dola.99@gmail.com>
2021-01-11 18:13:43 +0700

    [Fix] Tính hợp google Analytics vào bài tập

--- pages/_document.js
+++ pages/_document.js
@@ -7,31 +7,27 @@
  * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
  */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { GA_TRACKING_ID } from '../lib/gtag'
+
+import { GA_TRACKING_ID } from '../lib/gtag';
+// <script data-ad-client='ca-pub-2616710485643965' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>;
 export default class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <script data-ad-client='ca-pub-2616710485643965' async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
-                  <!-- Global site tag (gtag.js) - Google Analytics -->
-                  <script async src="https://www.googletagmanager.com/gtag/js?id=G-TJXXCXXME2"></script>
-                  <script
-                    async
-                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                  />
-                  <script
-                    dangerouslySetInnerHTML={{
-                      __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-                    }}
-                  />
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_TRACKING_ID}', {
+                            page_path: window.location.pathname,
+                          });`,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
