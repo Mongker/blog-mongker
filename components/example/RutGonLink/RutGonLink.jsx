5d4d1d3466aa1b1ba56eb97c8a7ee47c80a48584
Mongker <levanmong.dola.99@gmail.com>
2021-01-11 15:19:58 +0700

    [New] Xây dựng web rút gọn link

---
+++ components/example/RutGonLink/RutGonLink.jsx
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2020 present, Lê Văn Mong.
+ * All rights reserved.
+ * @author Mongker 11/01/2021
+ * @email: levanmong.dola.99@gmail.com
+ * @student_code: 68DCHT20091
+ * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
+ */
+
+import React from 'react';
+import { Button } from 'antd';
+// import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+
+// Note: Xem xét đưa API này vào trong web: https://api.baomoi.com/v1/webs/get/content-by-region?platform=1&short_name=HA_NOI&img_size=w200_r1x1&start=0&size=10
+function RutGonLink({ name, url }) {
+    const router = useRouter();
+    const [windowSize, setWindowSize] = React.useState({
+        heightApp: null,
+        widthApp: null,
+    });
+
+    React.useEffect(() => {
+        setWindowSize({
+            heightApp: window.innerHeight,
+            widthApp: window.innerWidth,
+        });
+    }, []);
+    const onNextLink = () => {
+        router.push(url);
+    };
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Button onClick={onNextLink}>Chuyển link</Button>
+            <iframe src={url} width={windowSize.widthApp} height={windowSize.heightApp} />
+        </div>
+    );
+}
+
+export default RutGonLink;
---
+++ pages/example/link/[url].js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2020 present, Lê Văn Mong.
+ * All rights reserved.
+ * @author Mongker 11/01/2021
+ * @email: levanmong.dola.99@gmail.com
+ * @student_code: 68DCHT20091
+ * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
+ */
+
+import axios from 'axios';
+import React from 'react';
+import { useRouter } from 'next/router';
+import RutGonLink from '../../../components/example/RutGonLink/RutGonLink';
+// import PropTypes from 'prop-types';
+
+// Note sau này phải tạo file API base để xử lý
+function getURL_API(url_compact) {
+    return (
+        axios
+            // .get(`https://server-mybook-1r1b2ebod.vercel.app/api/url/${url_compact}`)
+            .get(`http://localhost:1999/api/url/${url_compact}`)
+            .then((res) => res.data)
+            .catch((error) => console.log(error))
+    );
+}
+export async function getStaticPaths() {
+    return { paths: [], fallback: true };
+}
+
+export async function getStaticProps(params) {
+    const res = await getURL_API(params.params.url);
+    if (!res) {
+        return {
+            notFound: false,
+        };
+    }
+    console.log('params: ', params);
+    console.log('res', res ? res.url : '[Error Server]');
+    return {
+        props: {
+            url: res ? res.url : '[Error Server]',
+            name: [params['params'].url],
+        },
+    };
+}
+
+function url(props) {
+    return <RutGonLink {...props} />;
+}
+
+export default url;
