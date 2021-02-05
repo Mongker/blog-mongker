9d6a6eaa9cff944de5baaaf8c9a0585a86047172
Mongker <levanmong.dola.99@gmail.com>
2021-02-05 10:41:17 +0700

    [FIX] Chỉnh lại SEO cho chị V

--- components/MetaView.jsx
+++ components/MetaView.jsx
@@ -16,17 +16,17 @@ function MetaView({ title, description, url, image, icon, name }) {
     return (
         <Head>
             <title>{title}</title>
-            <link rel='icon' href={icon} />
             <meta name='robots' content='index, follow' />
-            <meta name='author' content={name} />
             {/* SEO google, facebook */}
-            <meta property='og:url' content={url} />
-            <meta property='og:type' content='article' />
-            <meta property='og:title' content={title} />
             <meta property='og:description' content={description} />
+            <meta property='og:url' content={url} />
+            <meta property='og:title' content={title} />
+            <meta property='og:type' content='article' />
             <meta property='og:image' content={image} />
             <meta property='og:image:width' content='600' />
             <meta property='og:image:height' content='315' />
+            <link rel='icon' href={icon} />
+            <meta name='author' content={name} />
         </Head>
     );
 }
@@ -40,10 +40,10 @@ MetaView.propTypes = {
 
 MetaView.defaultProps = {
     name: 'Quỳnh Vân',
-    icon: 'https://scontent.fhan2-6.fna.fbcdn.net/v/t1.0-9/131338842_2800734746872256_7450134103626634553_n.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=QWziK15JU0gAX8ttkzY&_nc_ht=scontent.fhan2-6.fna&oh=995cfd23ad9f9018c8998741da62d7c4&oe=601818BD',
+    icon: 'https://firebasestorage.googleapis.com/v0/b/blog-mongker.appspot.com/o/FaviconWeb%2Ffavicon.ico?alt=media&token=4fe2f924-62b4-44ee-8c18-5097a29adb2d',
     title: 'Mỹ phẩm Quỳnh Vân',
     url: 'https://mongker.cf/',
-    image: 'https://scontent.fhan2-6.fna.fbcdn.net/v/t1.0-9/131338842_2800734746872256_7450134103626634553_n.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=QWziK15JU0gAX8ttkzY&_nc_ht=scontent.fhan2-6.fna&oh=995cfd23ad9f9018c8998741da62d7c4&oe=601818BD',
+    image: 'https://firebasestorage.googleapis.com/v0/b/blog-mongker.appspot.com/o/FaviconWeb%2Ffavicon.ico?alt=media&token=4fe2f924-62b4-44ee-8c18-5097a29adb2d',
     description: 'Mua mỹ phẩm chính hãng - Mỹ phẩm nhập trực tiếp từ Hàn Quốc, Mỹ, Pháp... Giá Tốt. Miễn phí giao hàng tận nơi với đơn hàng trên 700K. Miễn phí hoàn trả trong 7 ngày. Tích luỹ điểm thưởng khi mua hàng tại trên website...',
 };
 
