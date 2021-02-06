293a7ef42c5f53254c5e2bee607a29e09a48a598
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2021-02-06 16:31:55 +0700

    [FIX] review link SEO 4

--- pages/blog/[news].js
+++ pages/blog/[news].js
@@ -45,7 +45,7 @@ export async function getStaticProps(params) {
 function news(props) {
     const data = {
         name: 'Blog Mongker',
-        icon: 'https://s240-ava-talk.zadn.vn/8/f/3/5/6/240/499c8cfa904f6c89e44aed82aab25b06.jpg',
+        icon: props.photo_news,
         title: props.title,
         url: `https://mongker.cf/${props.uid}`,
         image: props.photo_news,
