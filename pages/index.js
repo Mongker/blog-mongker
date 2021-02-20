57dc7f67939d94cd3b42124786feb857abc097e7
Mongker <levanmong.dola.99@gmail.com>
2021-02-20 17:41:14 +0700

    MongLV: Fix 5 (chán)

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,10 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import absoluteUrl from 'next-absolute-url';
 
 // JSX
 import VanKelly from '../components/VanKelly';
-// export async function getStaticProps() {
-//     const db = await myDB.connect({
-//         host: process.env.DB_HOST,
-//         username: process.env.DB_USER,
-//         password: process.env.DB_PASS,
-//     })
-//     // ...
-// }
-function Index({origin}) {
-    const hostname = typeof window !== 'undefined' && window.location.hostname ? window.location.hostname : '';
-    console.log('hostname', hostname);
-    let component = <div>Mong 2</div>;
-    switch (hostname) {
-        case 'vankelly.cf':
-            component = <VanKelly />;
-            break;
-        case 'http://localhost:2000':
-            component = <div>Mong</div>;
-            break;
-        default:
-            component = <div>Mong 2</div>;
-            break;
-    }
+
+function Index({}) {
     return <VanKelly />;
 }
 
