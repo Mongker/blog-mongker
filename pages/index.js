aa57920fd27f33c0e8ed0560fa5391b0a16159e5
Mongker <levanmong.dola.99@gmail.com>
2021-02-20 16:45:39 +0700

    MongLV: Fix

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import absoluteUrl from 'next-absolute-url';
-import { useRouter } from 'next/router';
 
 // JSX
 import VanKelly from '../components/VanKelly';
 
 function Index() {
     const { origin } = absoluteUrl();
-    console.log('origin', origin);
-    let component = <React.Fragment />;
+    let component = <div>NUL</div>;
     switch (origin) {
         case 'https://vankelly.cf':
             component = <VanKelly />;
