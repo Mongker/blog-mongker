7bfa7b27834894d706200c7f280bfa11331af4ef
Mongker <levanmong.dola.99@gmail.com>
2021-02-20 17:15:35 +0700

    MongLV: Fix 4

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import absoluteUrl from 'next-absolute-url';
 
 // JSX
 import VanKelly from '../components/VanKelly';
-export async function getStaticProps() {
-    const db = await myDB.connect({
-        host: process.env.DB_HOST,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-    })
-    // ...
-}
+// export async function getStaticProps() {
+//     const db = await myDB.connect({
+//         host: process.env.DB_HOST,
+//         username: process.env.DB_USER,
+//         password: process.env.DB_PASS,
+//     })
+//     // ...
+// }
 function Index({origin}) {
     const hostname = typeof window !== 'undefined' && window.location.hostname ? window.location.hostname : '';
     console.log('hostname', hostname);
