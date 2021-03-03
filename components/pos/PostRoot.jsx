1132d822c8e883f4b0ab91e0a7d2feb9235c9f7b
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2021-03-04 00:20:50 +0700

    SEO meta cho website 2

--- components/pos/PostRoot.jsx
+++ components/pos/PostRoot.jsx
@@ -18,8 +18,8 @@ function PostRoot() {
         icon: 'https://firebasestorage.googleapis.com/v0/b/cms-post-bank.appspot.com/o/cms-post-bank%2Fpngtree-vector-bank-icon-png-image_4015429.jpg?alt=media&token=3b0acd31-2667-46e2-bdfa-f3cbb9a367ea',
         title: 'Đáo thẻ tín dụng Thanh Hóa - 0888.589.678',
         url: 'https://anhduc.cf/',
-        image: 'https://firebasestorage.googleapis.com/v0/b/cms-post-bank.appspot.com/o/cms-post-bank%2F%E1%BA%A3nh%20n%E1%BB%81n.jpg?alt=media&token=e2df8ea7-79b5-48e5-8a06-b8deb449304a',
-        description: 'Dịch vụ đáo thẻ tín dụng Anh Đức (0888.589.678) đã hoạt động được 5 năm và có được sự tin tưởng của hơn 200k người dùng toàn Thanh Hóa, với sự phục vụ hết mình chúng tôi đem đến cho bạn những giá trị mà bạn cần. Hãy cầm điện thoại và gọi ngay 0888.589.678 để nhận những gì bạn cần nào !',
+        image: 'https://firebasestorage.googleapis.com/v0/b/cms-post-bank.appspot.com/o/cms-post-bank%2F%E1%BA%A3nh%20n%E1%BB%81n.jpg?alt=media&token=3cf3f243-7a49-467b-b2ab-e12a8903ff60',
+        description: 'Dịch vụ đáo thẻ tín dụng Anh Đức (0888.589.678) đã hoạt động được \\/n 5 năm và có được sự tin tưởng của hơn 200k người dùng toàn Thanh Hóa, với sự phục vụ hết mình chúng tôi đem đến cho bạn những giá trị mà bạn cần. Hãy cầm điện thoại và gọi ngay 0888.589.678 để nhận những gì bạn cần nào !',
     }
     return(
         <React.Fragment>
