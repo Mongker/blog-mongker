3ad3152d850d84a26cac139e3efaf08d7d391fe3
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2021-03-04 00:43:33 +0700

    SEO meta cho website 3

--- components/pos/PostRoot.jsx
+++ components/pos/PostRoot.jsx
@@ -19,7 +19,7 @@ function PostRoot() {
         title: 'Đáo thẻ tín dụng Thanh Hóa - 0888.589.678',
         url: 'https://anhduc.cf/',
         image: 'https://firebasestorage.googleapis.com/v0/b/cms-post-bank.appspot.com/o/cms-post-bank%2F%E1%BA%A3nh%20n%E1%BB%81n.jpg?alt=media&token=3cf3f243-7a49-467b-b2ab-e12a8903ff60',
-        description: 'Dịch vụ đáo thẻ tín dụng Anh Đức (0888.589.678) đã hoạt động được \\/n 5 năm và có được sự tin tưởng của hơn 200k người dùng toàn Thanh Hóa, với sự phục vụ hết mình chúng tôi đem đến cho bạn những giá trị mà bạn cần. Hãy cầm điện thoại và gọi ngay 0888.589.678 để nhận những gì bạn cần nào !',
+        description: 'Dịch vụ đáo thẻ tín dụng Anh Đức (0888.589.678) đã hoạt động \n được 5 năm và có được sự tin tưởng của hơn 200k người dùng toàn Thanh Hóa, \n với sự phục vụ hết mình chúng tôi đem đến cho bạn những giá trị mà bạn cần. \n Hãy cầm điện thoại và gọi ngay 0888.589.678 để nhận những gì bạn cần nào !',
     }
     return(
         <React.Fragment>
