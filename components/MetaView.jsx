88772fb2b4566ebf907163c28016692a56f40930
monglv <levanmong.dola.99@gmail.com>
2021-05-31 14:29:04 +0700

    Chinh lai ten web

--- components/MetaView.jsx
+++ components/MetaView.jsx
@@ -45,9 +45,9 @@ MetaView.propTypes = {
 };
 
 MetaView.defaultProps = {
-    name: 'Blog Mongker',
+    name: 'Mongker Media',
     icon: 'https://s240-ava-talk.zadn.vn/8/f/3/5/6/240/499c8cfa904f6c89e44aed82aab25b06.jpg',
-    title: 'Lê Văn Mong - Blogger',
+    title: 'Mongker Media',
     url: 'https://mongker.cf/',
     image: 'https://s240-ava-talk.zadn.vn/8/f/3/5/6/240/499c8cfa904f6c89e44aed82aab25b06.jpg',
     description: 'Website là nơi chia sẽ kiến thức lập trình theo góc nhìn của mình, mong những kiến thức của mình có thể giúp đỡ các bạn trong công cuộc kiến thiết bản thân của mình.',
